676016e99248d1a016e40d7859b30d7ae1a268cc
juanbai_shu <3589549384@qq.com>
2026-04-29 11:10:24 +0800

    最小生成树 kruskal算法代码模板
    递增着遍历所有的边 --> 判断两端是否可连接 --> 得到结果
    思路很简单，要结合并查集，并且 使用边集数组，方便存储这种 通过边访问两端的关系

---
+++ kruskal算法.cpp
@@ -0,0 +1,52 @@
+// 递增边 --> 祖先关系 --> 连接两点
+#include <iostream>
+#include <algorithm>
+#include <cstring>
+using namespace std;
+int n, m;
+// 使用边集数组存图，因为便于表示顶点与边的关系
+struct edge{
+	int x, y;
+	int w;
+} e[10008];
+bool cmp(const edge &a, const edge &b) {
+	return a.w < b.w;
+}
+
+int fa[10008];		// 并查集组别
+int find(int x) {			// 递归找根节点
+	if (fa[x] == x) {		// 找到了根节点
+		return x;
+	}	// 终止条件
+	return fa[x] = find(fa[x]);		// 路径压缩，子节点都直接连到根节点
+}
+
+int kruskal() {
+	int sum = 0;
+	for (int i = 1; i <= n; ++i) fa[i] = i;		// 初始时，自身为自身组别
+	sort(e+1, e + m + 1, cmp);		// 递增排序，左闭右开
+
+	for (int i = 1; i <= m; ++i) {		// 需找 n-1 条边，构建最小生成树
+		// 判断边两端是否可连
+		int x = find(e[i].x);
+		int y = find(e[i].y);
+		if (x == y) continue;		// 不能连情况
+		fa[x] = y;					// x 那组的祖先变成 y，谁当祖先都行
+		sum += e[i].w;
+	}
+	return sum;
+}
+
+int main() {
+	cin >> n >> m;
+	int x, y, wi;
+	for (int i = 1; i <= m; ++i) {
+		cin >> x >> y >> wi;
+		e[i] = { x, y, wi };
+	}
+
+	int sum = kruskal();
+	cout << sum;
+
+	return 0;
+}
