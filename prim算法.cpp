26b5a0c175f0ee44a1519b4febd542e3cec66a17
juanbai_shu <3589549384@qq.com>
2026-04-30 11:56:41 +0800

    更新了堆优化，prim算法时间复杂度降到了 O(nlogn)

--- prim算法.cpp
+++ prim算法.cpp
@@ -3,6 +3,9 @@
 #include <iostream>
 #include <cstring>
 #include <climits>
+#include <queue>
+#include <vector>
+#include <utility>		// pair
 #define inf INT_MAX
 using namespace std;
 int n, m;
@@ -23,43 +26,82 @@ void add(int x, int y, int wi) {
 	cnt++;
 }
 
+//				优化前的prim算法，时间复杂度为 O(n^2)
+//int prim() {
+//	for (int i = 1; i <= n; ++i) dis[i] = inf;
+//	int sum = 0;
+//	int count = 0;
+//	int mi = inf;
+//	int now;		// 当前顶点位置
+//	dis[1] = 0;		// 不存在入度边，因此为 0
+//	vis[1] = 1;
+//	for (int i = h[1]; i != -1; i = e[i].nex) {					// 初始化第一个
+//		int u = e[i].to;
+//		dis[u] = min(dis[u], e[i].w);
+//	}
+//
+//	while (count < n - 1) {										// 把剩下的遍历完
+//		mi = inf;
+//
+//		for (int i = 1; i <= n; ++i) {							// 全局找下一点
+//			if (vis[i] == 0 && dis[i] < mi) {
+//				now = i;
+//				mi = dis[i];
+//			}
+//		}
+//
+//		if (mi == inf) return -1;								// 说明路不通，不是连通图
+//		vis[now] = 1;
+//		count++;
+//		sum += mi;
+//
+//		for (int i = h[now]; i != -1; i = e[i].nex) {			// 辐射更新 dis
+//			int u = e[i].to;
+//			if (vis[u] == 0 && e[i].w < dis[u]) {
+//				dis[u] = e[i].w;
+//			}
+//		}
+//
+//	}
+//
+//
+//	return sum;
+//}
+
+
+//					优化后的prim算法，使用堆优化，时间复杂度为 O(nlogn)
+typedef pair<int, int> PII;
+priority_queue<PII, vector<PII>, greater<> > q;		// 递增队列
+
 int prim() {
 	for (int i = 1; i <= n; ++i) dis[i] = inf;
+	int count = 0;		// 找到顶点数，初始点 1 算进去了，直到 count == n
 	int sum = 0;
-	int count = 0;
-	int mi = inf;
-	int now;		// 当前顶点位置
-	dis[1] = 0;		// 不存在入度边，因此为 0
-	vis[1] = 1;
-	for (int i = h[1]; i != -1; i = e[i].nex) {					// 初始化第一个
-		int u = e[i].to;
-		dis[u] = min(dis[u], e[i].w);
-	}
+	PII p;
+	p = { 0, 1 };		// 根据权值排序，对应位置便于使用
+	q.push(p);
+	dis[1] = 0;			// 首次要使用 sum += dis[1];
 
-	while (count < n - 1) {										// 把剩下的遍历完
-		mi = inf;
-
-		for (int i = 1; i <= n; ++i) {							// 全局找下一点
-			if (vis[i] == 0 && dis[i] < mi) {
-				now = i;
-				mi = dis[i];
-			}
-		}
-
-		if (mi == inf) return -1;								// 说明路不通，不是连通图
-		vis[now] = 1;
+	while (!q.empty()) {
+		p = q.top();
+		q.pop();
+		int x = p.second;		// x == 该辐射点索引
+		if (vis[x] == 1) continue;		// 防止未使用前，重复入队
+		vis[x] = 1;				// 将使用的点设定一下
+		sum += dis[x];
 		count++;
-		sum += mi;
+		if (count == n) break;
 
-		for (int i = h[now]; i != -1; i = e[i].nex) {			// 辐射更新 dis
-			int u = e[i].to;
-			if (vis[u] == 0 && e[i].w < dis[u]) {
-				dis[u] = e[i].w;
+		for (int i = h[x]; i != -1; i = e[i].nex) {
+			int y = e[i].to;
+			if (vis[y] == 0 && e[i].w < dis[y]) {
+				dis[y] = e[i].w;
+				p = { dis[y], y };
+				q.push(p);			// 辐射到的都进
 			}
 		}
-
 	}
-
+	if (count < n) return -1;		// 未连通
 
 	return sum;
 }
